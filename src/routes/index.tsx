29dd2c70a64abc503460afc4d3ec5620d802722d
Nguyen Tuan Anh <tuanhaoggg@gmail.com>
2025-08-28 16:56:26 +0700

    feat(styles): add SCSS abstracts for colors and backgrounds

---
+++ src/routes/index.tsx
@@ -0,0 +1,5 @@
+import { createBrowserRouter } from 'react-router';
+
+import LadingPage from '@/pages/lading-page/Lading';
+
+export const router = createBrowserRouter([{ path: '/', element: <LadingPage /> }]);
